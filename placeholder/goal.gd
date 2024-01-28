7b854af648a4d6e7bc76eac625a7b622ec87070e
Andrew Freeman <andrewfreeman234@gmail.com>
2024-01-27 18:18:57 -0700

    feat: Add 20x20 tileset

--- placeholder/goal.gd
+++ placeholder/goal.gd
@@ -3,5 +3,8 @@ class_name Goal extends Node2D
 @export var level_to_load : int
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_class("CharacterBody2D"):
+	print("AREA ENTERED")
+	# if body.is_class("Player"):
+	if body is Player:
+		print("EMITTING LEVEL LOAD")
 		SignalManager.level_changed.emit(level_to_load)

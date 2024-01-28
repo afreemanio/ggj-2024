47aa68ef1ce761920633f20b59db24aefc6aa68e
Andrew Freeman <andrewfreeman234@gmail.com>
2024-01-27 17:06:27 -0700

    feat: Add path return behaviour

---
+++ entities/enemies/enemy_found_noise_source_quick_search_state.gd
@@ -0,0 +1,60 @@
+class_name EnemyFoundNoiseQuickSearchState extends State
+
+# Dependencies - if it doesn't have these, it won't work
+@export var actor: Enemy
+@export var animator: AnimatedSprite2D
+@export var vision_cast: RayCast2D
+
+signal found_player
+# @export var vision_renderer: Polygon2D
+# @export var alert_color: Color
+
+# @export_group("Rotation")
+# @export var is_rotating = false
+# @export var rotation_speed = 0.1
+# @export var rotation_angle = 90
+
+# @export_group("Movement")
+# @export var move_on_path: PathFollow2D
+# @export var movement_speed = 0.1
+@onready var pos_start = position.x
+
+@onready var rot_start = rotation
+var heard_sound_location: Vector2
+@export var max_speed_multiplier: float = 0.5
+@export var acceleration_multiplier: float = 0.5
+@export var animation_player: AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_physics_process(false)
+	pass # Replace with function body.
+
+func _enter_state() -> void:
+	print("ENTER STATE FOUND NOISE QUICK SEARCH")
+	print(animation_player.get_animation_list())
+	animation_player.play("search_left_and_right")
+	set_physics_process(true)
+
+
+func _exit_state() -> void:
+	print("EXIT STATE FOUND NOISE QUICK SEARCH")
+	animation_player.stop()
+	set_physics_process(false)
+
+
+func _physics_process(delta: float) -> void:
+	# var direction = actor.global_position.direction_to(heard_sound_location)
+	# actor.velocity = actor.velocity.move_toward(
+	# 	direction * actor.max_speed * max_speed_multiplier, actor.acceleration * delta * acceleration_multiplier
+	# )
+	# actor.move_and_slide()
+	# actor.look_at(global_position + actor.velocity)
+
+	# actor slowly looks left, slowly looks right, then navs back to original path
+	# rotation.y = lerp_angle(rotation.y, _target_angle, delta * _rotation_amount)
+	# tween.tween_property(pivot, "transform", pivot.transform.rotated(axis, PI/2), 0.5)
+	pass
+
+
--- entities/enemies/enemy_navigate_back_to_path_state.gd
+++ entities/enemies/enemy_navigate_back_to_path_state.gd
@@ -1,22 +1,11 @@
-class_name EnemyFollowNoiseState extends State
+class_name EnemyNavigateBackToPathState extends State
+
 
 # Dependencies - if it doesn't have these, it won't work
 @export var actor: Enemy
 @export var animator: AnimatedSprite2D
-@export var vision_cast: RayCast2D
 
-signal found_noise
-# @export var vision_renderer: Polygon2D
-# @export var alert_color: Color
-
-# @export_group("Rotation")
-# @export var is_rotating = false
-# @export var rotation_speed = 0.1
-# @export var rotation_angle = 90
-
-# @export_group("Movement")
-# @export var move_on_path: PathFollow2D
-# @export var movement_speed = 0.1
+signal found_path
 @onready var pos_start = position.x
 
 @onready var rot_start = rotation
@@ -32,15 +21,37 @@ func _ready():
 	pass # Replace with function body.
 
 
-func create_noise_location_node(creation_location: Vector2) -> void:
+func find_closest_abs_pos_to_path(
+	path: Path2D,
+	global_pos: Vector2
+):
+	var curve: Curve2D = path.curve
+
+	# transform the target position to local space
+	var path_transform: Transform2D = path.global_transform
+	var local_pos: Vector2 = global_pos * path_transform
+
+	# get the nearest offset on the curve
+	var offset: float = curve.get_closest_offset(local_pos)
+
+	# get the local position at this offset
+	var curve_pos: Vector2 = curve.sample_baked(offset, true)
+
+	# transform it back to world space
+	curve_pos = path_transform * curve_pos
+
+	return curve_pos
+
+
+func create_path_location_node(creation_location: Vector2) -> void:
 	if heard_sound_location != null:
 		heard_sound_location.queue_free()
 	heard_sound_location = StaticBody2D.new()
-	heard_sound_location.name = "Heard Sound Location"
+	heard_sound_location.name = "Home Path Location"
 	heard_sound_location.position = creation_location
 	heard_sound_location.add_to_group("noise_location")
 	heard_sound_location.set_collision_layer_value(1, false)
-	heard_sound_location.set_collision_layer_value(5, true)
+	heard_sound_location.set_collision_layer_value(6, true)
 	# heard_sound_location.set_collision_mask_value(3, true)
 	create_collision_on_location_node()
 	get_tree().get_root().add_child(heard_sound_location)
@@ -55,25 +66,25 @@ func create_collision_on_location_node() -> void:
 	heard_sound_location.add_child(collision)
 
 
-func delete_noise_location_node() -> void:
+func delete_path_location_node() -> void:
 	heard_sound_location.queue_free()
 
 
 # Implements enter state:
 func _enter_state() -> void:
-	print("ENTER STATE NOISE FOLLOW")
-	# heard_sound_location = actor.heard_sound_location_buffer
-	create_noise_location_node(actor.heard_sound_location_buffer)
+	print("ENTER STATE PATH RETURN")
+	# Find location closest to home path
+	# var curve_pos = find_closest_abs_pos_to_path(actor.move_on_path.get_parent(), actor.global_position)
+	var curve_pos = actor.move_on_path.position
+	print("Trying to nav to: " + str(curve_pos))
+	create_path_location_node(curve_pos)
 	actor.heard_sound_location_buffer = Vector2.ZERO
 	set_physics_process(true)
-	# animator.play("move")
-	# if actor.velocity == Vector2.ZERO:
-	# 	actor.velocity = Vector2.RIGHT.rotated(randf_range(0, TAU)) * actor.max_speed
 
 
 func _exit_state() -> void:
-	print("EXIT STATE NOISE FOLLOW")
-	delete_noise_location_node()
+	print("EXIT STATE PATH RETURN")
+	delete_path_location_node()
 	set_physics_process(false)
 
 

272b83f13f576181ccd8a8247c3074dd32060dd1
Lane McMartin <37342668+LaneMcMartin@users.noreply.github.com>
2024-01-27 20:21:49 -0700

    feat: Added player sprite

--- player/Player.gd
+++ player/Player.gd
@@ -1,11 +1,12 @@
 class_name Player extends CharacterBody2D
 
-@export var speed : int = 300
+@export var speed : int = 150
+
+var previously_moving_right : bool = false
 
 func _ready():
-	## TODO: Maybe can be removed
-	# %PhantomCamera2D.set_follow_target_node(%CharacterBody2D)
-	%PhantomCamera2D.is_active()
+	%AnimatedSprite2D.play("idle")
+	%AnimatedSprite2D.flip_h = true
 	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("SPACE"):
@@ -16,5 +17,29 @@ func _physics_process(delta):
 
 ## Get player input and update the movement velocity vector
 func _update_movement():
+	# Get movement direction
 	var movement_direction : Vector2 = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	
+	# Set sprite
+	if movement_direction.x != 0 and movement_direction.y == 0:
+		%AnimatedSprite2D.play("walk_forward")
+		print("Forward")
+	elif movement_direction.y < 0:
+		%AnimatedSprite2D.play("walk_up")
+		print("Up")
+	elif movement_direction.y > 0:
+		%AnimatedSprite2D.play("walk_down")
+		print("Down")
+	else:
+		%AnimatedSprite2D.play("idle")
+		print("Idle")
+		
+	# Flip sprite and cache x-direction
+	if previously_moving_right and movement_direction.x < 0:
+		%AnimatedSprite2D.flip_h = false
+		previously_moving_right = false
+	if !previously_moving_right and movement_direction.x > 0:
+		%AnimatedSprite2D.flip_h = true
+		previously_moving_right = true
+		
 	velocity = movement_direction * speed
